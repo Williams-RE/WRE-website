8e23e647bef8dadcfe82b1951c446b61cdded734
Shelby-Simpson <shelbysimpson98@gmail.com>
2021-08-26 17:33:34 -0400

    cloned repo, added filler paragraphs, and put all modules on same page for scrolling

--- src/App.js
+++ src/App.js
@@ -1,60 +1,53 @@
 import './App.css';
 import logo from './images/wre-logo.png';
-import {
-  Route,
-  NavLink,
-  HashRouter
-} from "react-router-dom";
-import LandingPage from './LandingPage/LandingPage';
+// import {
+//   Route,
+//   NavLink,
+//   HashRouter
+// } from "react-router-dom";
+// import LandingPage from './LandingPage/LandingPage';
 import BuyAHome from './BuyAHome/BuyAHome';
 import SellAHome from './SellAHome/SellAHome';
-import FindAnAgent from './FindAnAgent/FindAnAgent';
+// import FindAnAgent from './FindAnAgent/FindAnAgent';
 import AboutUs from './AboutUs/AboutUs';
-import "typeface-roboto";
 
 function App() {
   return (
     <div class="main">
 
-<img class="pull-right"
-            src="https://www.userbenchmark.com/resources/img/wri/creatives/assets/flame.gif" alt = "fire">
+      <img class="pull-right"
+        src="https://www.userbenchmark.com/resources/img/wri/creatives/assets/flame.gif" alt = "fire">
 
-            </img>
+      </img>
       
       <div class="background">
       </div>
 
       <div class="navigation-bar">
         
-        <HashRouter>
-          <nav class = "navigation">
-            <NavLink activeClassName="selected-navlink" to="/">
-              <img src={ logo } alt="logo" />
-            </NavLink>
-            <NavLink activeClassName="selected-navlink" to="/Buy">
-              <span class="navlink-text">Buy a Home</span>
-            </NavLink> 
-            <NavLink activeClassName="selected-navlink" to="/Sell">
-              <span class="navlink-text">Sell a Home</span>
-            </NavLink>
-            <NavLink activeClassName="selected-navlink" to="/FindAgent">
-              <span class="navlink-text">Find an Agent</span>
-            </NavLink> 
-            <NavLink activeClassName="selected-navlink" to="/About">
-              <span class="navlink-text">About Us</span>
-            </NavLink>
-          </nav>
-          
-          <div class="navigation-content">
-            <Route exact path="/" component={ LandingPage } />
-            <Route path="/Buy" component={ BuyAHome } />
-            <Route path="/Sell" component={ SellAHome } />
-            <Route path="/FindAgent" component={ FindAnAgent } />
-            <Route path="/About" component={ AboutUs } />
-          </div>
-        </HashRouter>
+        <nav class = "navigation">
+          <a href="">
+            <img src={ logo } alt="logo" />
+          </a>
+          <a href="">
+            <span class="navlink-text">Buy a Home</span>
+          </a> 
+          <a href="">
+            <span class="navlink-text">Sell a Home</span>
+          </a>
+          <a href="">
+            <span class="navlink-text">Find an Agent</span>
+          </a> 
+          <a href="">
+            <span class="navlink-text">About Us</span>
+          </a>
+        </nav>
 
       </div>
+
+      <BuyAHome />
+      <SellAHome />
+      <AboutUs />
 
     </div>
   );
