ebd3b172490ad3f0025817134766b1ab829e5b0d
Shelby-Simpson <shelbysimpson98@gmail.com>
2021-08-26 20:07:34 -0400

    Added scroll to page components on navbar click

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import './App.css';
+import { useRef } from 'react';
 import logo from './images/wre-logo.png';
 // import {
 //   Route,
 //   NavLink,
 //   HashRouter
 // } from "react-router-dom";
-// import LandingPage from './LandingPage/LandingPage';
+import LandingPage from './LandingPage/LandingPage';
 import BuyAHome from './BuyAHome/BuyAHome';
 import SellAHome from './SellAHome/SellAHome';
 // import FindAnAgent from './FindAnAgent/FindAnAgent';
 import AboutUs from './AboutUs/AboutUs';
 
+
+
 function App() {
+  const landingPageRef = useRef();
+  const buyAHomeRef = useRef();
+  const sellAHomeRef = useRef();
+  const aboutUsRef = useRef();
+
+  function scrollToLandingPage() {
+    landingPageRef.current.scrollIntoView({ behavior: 'smooth'});
+  }
+  
+  function scrollToBuyAHome() {
+    buyAHomeRef.current.scrollIntoView({ behavior: 'smooth'});
+  }
+  
+  function scrollToSellAHome() {
+    sellAHomeRef.current.scrollIntoView({ behavior: 'smooth'});
+  }
+  
+  function scrollToAboutUs() {
+    aboutUsRef.current.scrollIntoView({ behavior: 'smooth'});
+  }
+
   return (
     <div class="main">
 
       <img class="pull-right"
         src="https://www.userbenchmark.com/resources/img/wri/creatives/assets/flame.gif" alt = "fire">
-
       </img>
       
       <div class="background">
@@ -26,28 +49,34 @@ function App() {
       <div class="navigation-bar">
         
         <nav class = "navigation">
-          <a href="">
+          <button onClick={scrollToLandingPage}>
             <img src={ logo } alt="logo" />
-          </a>
-          <a href="">
+          </button>
+          <button onClick={scrollToBuyAHome}>
             <span class="navlink-text">Buy a Home</span>
-          </a> 
-          <a href="">
+          </button> 
+          <button onClick={scrollToSellAHome}>
             <span class="navlink-text">Sell a Home</span>
-          </a>
-          <a href="">
-            <span class="navlink-text">Find an Agent</span>
-          </a> 
-          <a href="">
+          </button>
+          <button onClick={scrollToAboutUs}>
             <span class="navlink-text">About Us</span>
-          </a>
+          </button>
         </nav>
 
       </div>
 
-      <BuyAHome />
-      <SellAHome />
-      <AboutUs />
+      <div class="page" ref={landingPageRef}>
+        <LandingPage />
+      </div>
+      <div class="page" ref={buyAHomeRef}>
+        <BuyAHome />
+      </div>
+      <div class="page" ref={sellAHomeRef}>
+        <SellAHome />
+      </div>
+      <div class="page" ref={aboutUsRef}>
+        <AboutUs />
+      </div>
 
     </div>
   );
--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -4,6 +4,26 @@ function LandingPage() {
     return (
         <div class="main4">
             <h1>Find Your Dream Home</h1>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque arcu, ultrices sit amet ullamcorper a, luctus ut ante. Duis ullamcorper non elit at euismod. Duis posuere nec justo sit amet vestibulum. 
+            Sed ex odio, molestie non faucibus vitae, consequat ut quam. Vestibulum porttitor metus a diam viverra, quis blandit tellus condimentum. In laoreet, justo id eleifend dictum, augue risus blandit orci, vel finibus ligula massa a sapien. 
+            Aliquam ullamcorper facilisis malesuada. Nam ullamcorper fermentum ipsum sed dictum. Quisque tincidunt commodo orci vitae rhoncus. Suspendisse fermentum magna et tempus porta. Vivamus blandit mi enim, sit amet pretium sem varius in. 
+            Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent id volutpat elit, in tristique mauris. Maecenas purus magna, ornare quis mauris ornare, aliquam sollicitudin tellus. Fusce sollicitudin aliquet commodo.
+
+            Vivamus condimentum tortor ut magna consectetur, quis eleifend leo efficitur. Praesent sed lorem sit amet sapien rutrum scelerisque sit amet id nunc. Donec porttitor porta metus, eu tempus leo tincidunt ac. Duis bibendum lectus sed orci scelerisque, eu posuere magna dapibus. 
+            Sed eleifend, risus non efficitur maximus, tellus metus fermentum turpis, vulputate suscipit lacus lorem id massa. Sed tempor urna euismod aliquet commodo. Proin consequat mi est, id elementum orci placerat sit amet. Vestibulum placerat nulla ut molestie aliquam. 
+            Maecenas venenatis arcu hendrerit enim mollis dapibus. Pellentesque eget dolor maximus, tempus massa nec, vestibulum ipsum. Vivamus fringilla vitae sapien ac faucibus. Mauris lobortis magna felis, non vulputate sapien sollicitudin sit amet. Nulla arcu dui, suscipit sed tempor a, volutpat sed augue. 
+            Nulla congue rutrum est ac placerat. Vestibulum ullamcorper, nisl vel dictum accumsan, dui mi sollicitudin eros, ut tristique ex ipsum nec odio. Nam faucibus cursus dolor ac fermentum.
+
+            Fusce pharetra feugiat pellentesque. Duis nibh ipsum, condimentum et hendrerit in, elementum pellentesque odio. Cras in nisl gravida, mollis magna at, sollicitudin libero. Vestibulum a mauris dolor. Aliquam ut quam in lorem commodo pretium. 
+            Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris eleifend maximus enim, ut varius lectus. Cras interdum sollicitudin pellentesque.
+
+            Quisque finibus, eros eu lacinia facilisis, ipsum orci efficitur mauris, vitae commodo mauris mauris ac nisi. Quisque eros purus, pulvinar eget urna at, ullamcorper commodo est. Sed hendrerit nunc leo, vel rhoncus augue dictum et. 
+            Nulla ut leo in turpis venenatis egestas. Etiam porta quis magna vitae maximus. Proin vehicula ex at commodo fermentum. Cras rutrum tellus eu gravida aliquet. Vivamus mollis ex ac leo mollis finibus.
+
+            Nunc metus diam, lobortis in eros id, pharetra viverra odio. Aenean tristique nisi sed nulla viverra, nec pulvinar augue hendrerit. Mauris tincidunt leo elit, blandit facilisis ante varius a. Duis egestas semper cursus. 
+            Pellentesque auctor tristique nulla quis lacinia. Sed vitae sapien purus. Nullam lorem ligula, finibus sit amet imperdiet ac, pretium at ligula. Phasellus mollis augue purus, a gravida dolor fringilla ut.
+            </p>
         </div>
     );
 }
