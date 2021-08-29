e4bf8fc5bf2a136ffd524d3cfc34edda86bebf2d
Shelby-Simpson <shelbysimpson98@gmail.com>
2021-08-29 07:20:44 -0400

    Created AgentProfile Component

---
+++ src/AboutUs/AgentProfile/AgentProfile.js
@@ -0,0 +1,30 @@
+import './AgentProfile.css';
+import React from 'react';
+
+ function AgentProfile(props) {
+     return (
+        <div class="agent-profile-container">
+            <div class="agent-profile-heading">
+                <h2>{props.agentProfileName}</h2>
+                <h3>{props.agentProfileTitle}</h3>
+            </div>
+            <div class="agent-profile-content">
+                <img src={props.agentProfileImagePath} alt={props.agentProfileName}/>
+                <p>
+                    {props.agentProfileBio}
+                </p>
+                <p>
+                    {props.agentProfileOfficeNumber}
+                </p>
+                <p>
+                    {props.agentProfileCellNumber}
+                </p>
+                <p>
+                    {props.agentProfileEmail}
+                </p>
+            </div>
+        </div>
+     );
+ }
+
+ export default AgentProfile;
