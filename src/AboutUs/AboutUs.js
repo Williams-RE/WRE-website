bfdaf0a99f5cf1c7bce665b81000ff3d3ada519a
axioms123 <61398302+axioms123@users.noreply.github.com>
2021-10-01 14:06:38 -0400

    styling

--- src/AboutUs/AboutUs.js
+++ src/AboutUs/AboutUs.js
@@ -58,12 +58,9 @@ function AboutUs() {
               agentProfileImagePath={agentProfileImagePath} agentProfileOfficeNumber={agentProfileOfficeNumber}
               agentProfileCellNumber={agentProfileCellNumber} agentProfileEmail={agentProfileEmail}
               />
-            <h1>About Us</h1>
+            <h1 class= 'about-us-text'> About us</h1>
             <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque arcu, ultrices sit amet ullamcorper a, luctus ut ante. Duis ullamcorper non elit at euismod. Duis posuere nec justo sit amet vestibulum. 
-            Sed ex odio, molestie non faucibus vitae, consequat ut quam. Vestibulum porttitor metus a diam viverra, quis blandit tellus condimentum. In laoreet, justo id eleifend dictum, augue risus blandit orci, vel finibus ligula massa a sapien. 
-            Aliquam ullamcorper facilisis malesuada. Nam ullamcorper fermentum ipsum sed dictum. Quisque tincidunt commodo orci vitae rhoncus. Suspendisse fermentum magna et tempus porta. Vivamus blandit mi enim, sit amet pretium sem varius in. 
-            Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent id volutpat elit, in tristique mauris. Maecenas purus magna, ornare quis mauris ornare, aliquam sollicitudin tellus. Fusce sollicitudin aliquet commodo.
+            Williams Real Estate was founded on principles of 
             </p>
             <Carousel responsive={responsive} slidesToSlide={5} containerClass="carousel" itemClass="carousel-agent" arrows={false} customButtonGroup={<CustomButtonGroupAsArrows />} renderButtonGroupOutside={true}>
                     <img class="agent-image" alt="Jacob" src={Jacob} onClick={() => openAgentProfileModal()}/>
